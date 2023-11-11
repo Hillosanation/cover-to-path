040ea4df9182c7cc6ad225ff1ef95db55406c408
Hillosanation <88579655+Hillosanation@users.noreply.github.com>
2023-11-12 00:16:10 +0800

    add some clarifying comments

--- cover-to-path.py
+++ cover-to-path.py
@@ -14,14 +14,17 @@ for row in csv.reader(open(args.csv_path, 'r')):
     InputCSV.append(row)
 # print(InputCSV)
 
+# grabs the list of fumens and convert to a one-page fumen for path format
 ungluedRow = assemble(InputCSV[0][1:])
 # print(ungluedRow)
 
 OutputCSV = []
-OutputCSV.append(["pattern", "solutionCount", "solutions", "unusedMinos", "fumens"]) #QoL, not read by strict-minimal
+OutputCSV.append(["pattern", "solutionCount", "solutions", "unusedMinos", "fumens"]) # QoL, not read by strict-minimal
 
 for row in InputCSV[1:]: 
     sequence = row[0]
+
+    # collect all fumens that can be built by the current queue according to cover
     SuccessFumens = []
     for element, fumen in zip(row[1:], ungluedRow):
         if (element == 'O'):
@@ -30,7 +33,7 @@ for row in InputCSV[1:]:
     OutputCSV.append([sequence, len(SuccessFumens), '', '', ";".join(SuccessFumens)])
 # print(OutputCSV)
 
-
+# use specified output file path, otherwise append "_to_path" to the input csv file
 OutputFilePath = ""
 if (args.output_file_path != ""):
     OutputFilePath = args.output_file_path
@@ -39,6 +42,8 @@ else:
     OutputFilePath = args.csv_path[:extentionPos] + "_to_path" + args.csv_path[extentionPos:]
 
 print(f"writing to file: {OutputFilePath}")
+
+# overwrites file if it exists
 OutputFileWriter = csv.writer(open(OutputFilePath, 'w', newline=''))
 for row in OutputCSV:
     OutputFileWriter.writerow(row)
